f2dd9c4da615dfb0e9c6cf3194d4446a8680ba15
parkgeunyoung1 <rmsdud3847@gmail.com>
2025-09-02 09:25:30 +0900

    Delete 2972.py

--- 2972.py
+++
@@ -1,9 +0,0 @@
-T = int(input())
-for tc in range(1, T+1):
-    num = list(map(int, input().split()))
-    num_sum = 0
-    for i in num:
-        if i % 2 == 1:
-            num_sum += i
-
-    print(f'#{tc} {num_sum}')
