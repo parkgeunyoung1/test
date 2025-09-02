537680fd64d784823b6a481967b6b75e46e6fc38
parkgeunyoung1 <rmsdud3847@gmail.com>
2025-09-02 09:27:37 +0900

    Update 2071.py

--- 2071.py
+++ 2071.py
@@ -1,11 +1,11 @@
-T = int(input())
-for tc in range(1, T+1):
-    num = list(map(int, input().split()))
-    num_sum = 0
-    count = 0
-    print(num)
-    for i in num:
-        num_sum += i
-        count += 1
-    result = round(num_sum / count)
+T = int(input())               
+for tc in range(1, T+1):    
+    num = list(map(int, input().split())) # 숫자열 리스트 할당
+    num_sum = 0  # 초기 숫자합
+    count = 0    # 숫자 갯수
+    for i in num:      # 숫자열 순회
+        num_sum += i     # 숫자 더하기
+        count += 1       # 숫자 갯수 새기
+    result = round(num_sum / count)   # 반올림
+ 
     print(f'#{tc} {result}')
