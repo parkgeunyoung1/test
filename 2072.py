68f2cc6c6c07a5b4cbb28f6c374411ca38081a49
parkgeunyoung1 <rmsdud3847@gmail.com>
2025-09-02 09:23:27 +0900

    Add files via upload

--- 2072.py
+++ 2072.py
@@ -1,9 +1,9 @@
-T = int(input())                      # T 삽입
-for tc in range(1, T+1):              # tc 생성
-    num = list(map(int, input().split()))  # 숫자열 리스트 생성
-    num_sum = 0          # 초기 숫자합
-    for i in num:        # 숫자열리스트 순회
-        if i % 2 == 1:   # 홀수(나머지가 1)찾기
-            num_sum += i # 홀수 더하기
- 
+T = int(input())                # 첫번째 줄 T 를 할당
+for tc in range(1, T+1):        # tc를 1~ T 까지 순회
+    num = list(map(int, input().split())) # 숫자열 할당
+    num_sum = 0   # 초기 숫자합
+    for i in num:   # 숫자열 순회
+        if i % 2 == 1: # 홀수인 숫자 일 때 숫자 더하기
+            num_sum += i
+
     print(f'#{tc} {num_sum}')
