8b9889ab5fc56207a3f588aebb15948cebd29d62
parkgeunyoung1 <rmsdud3847@gmail.com>
2025-09-02 09:34:05 +0900

    Update 1983.py

--- 1983.py
+++ 1983.py
@@ -2,7 +2,7 @@ T = int(input())
 grades = ["A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0"]                      # T 삽입
 for tc in range(1, T+1):                 # tc 생성
     N, K = map(int,input().split())       # N, K 값 부여      
-    scores = []
+    scores = []              # (학생, 점수) 리스트를 얻기 위해 빈 리스트를 부여
     for i in range(1, N + 1):
         mid, fin, hw = map(int, input().split())
         total = mid * 0.35 + fin * 0.45 + hw * 0.20
@@ -19,12 +19,12 @@ for tc in range(1, T+1):                 # tc 생성
     # --- K번째 학생 순위 찾기 ---
     target_total = 0
     for stu, total in scores:
-        if stu == K:
+        if stu == K:               #  K번째 학생 점수 찾기
             target_total = total
             break
 
     rank_index = 0
-    for _, total in scores:
+    for _, total in scores:                  # K번째 학생 점수보다 큰 학생들의 수를 세어 순위 찾기
         if total > target_total:
             rank_index += 1
     # --- 학점 계산 ---
@@ -32,3 +32,4 @@ for tc in range(1, T+1):                 # tc 생성
     grade_idx = rank_index // bucket
 
     print(f"#{t} {grades[grade_idx]}")
+
